a126c48a2d3a0d05e006ce990ef183101b49360a
dregs01 <jud828479332@gmail.com>
2024-06-04 22:29:59 +0800

    second

--- randomtest.py
+++ randomtest.py
@@ -1,12 +1,22 @@
 import random
-x=random.randint (1,100)
+
+start = input ('輸入開始範圍:')
+end = input ('輸入結束範圍')
+start = int(start)
+end=int(end)
+
+x=random.randint (start,end)
+count = 0
 while True:
+    count += 1
     num = input('請猜數字:')
     num = int(num)
     if num == x :
         print ('你猜中了!')
+        print ('這是您猜的第',count,'次')
         break
     elif num > x :
         print ('比數字小')
     else :
         print ('比數字大')
+    print ('這是您猜的第',count,'次')
